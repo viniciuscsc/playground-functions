dd95decc566a115498ff7b6f88601d1a8463b293
cunhavcs <cunha.vcs@gmail.com>
2022-08-30 21:08:46 -0300

    resolve requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,43 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  let phoneNumber = "";
+  let currentCount = 0;
+
+  if (numbers.length !== 11){
+    return "Array com tamanho incorreto.";
+  };
+
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] < 0 || numbers[index] > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
+    };
+
+    for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+      if (numbers[index] === numbers[index2]) {
+        currentCount += 1;
+      };
+    };
+
+    if (currentCount >= 3) {
+      return "não é possível gerar um número de telefone com esses valores";
+    };
+    currentCount = 0;  
+  
+    switch (index) {
+      case 0:
+        phoneNumber += "(" + numbers[index];
+        break;
+      case 2:
+        phoneNumber += ") " + numbers[index];
+        break;
+      case 7:
+        phoneNumber += "-" + numbers[index];
+        break;
+      default:
+        phoneNumber += numbers[index];
+    };
+  };
+  return phoneNumber;
 }
 
 // Desafio 12
